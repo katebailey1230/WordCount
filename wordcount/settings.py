d89156ac194e92d5078b8fe84420762a48224639
Kate <katebailey1230@gmail.com>
2018-09-03 15:10:40 +0000

    updates

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vrc5u-br5liniq+gu1^sfrkvj75p$r5yzh!dk5i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nola321.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/nola321/WordCount/static'
